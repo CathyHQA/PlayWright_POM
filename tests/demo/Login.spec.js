cea6278229131feb5e5071bbeed4c5ecfce2c8b8
Cathy <Cathy.hettison@gmail.com>
2025-02-22 01:34:14 -0800

    2nd Commit

--- tests/demo/Login.spec.js
+++ tests/demo/Login.spec.js
@@ -1,16 +1,38 @@
 import { test, expect } from '@playwright/test';
 import {LoginPage} from '../../pages/login'
-
-test('test', async ({ page }) => {
+//Verify loging Pass
+test('LoginSucess', async ({ page }) => {
 
   const Login = new LoginPage(page)
   await Login.gotoLoginPage()
   await Login.login('Cathy.hettison@gmail.com','Passis@123')
+  await expect(page).toHaveTitle('My Account')
+ 
+});
+//Verify loging Fail
+test('LoginFail', async ({ page }) => {
 
-  //await page.goto('https://magento.softwaretestingboard.com/customer/account/login');
-  //await page.getByRole('textbox', { name: 'Email*' }).click();
-  //await page.getByRole('textbox', { name: 'Email*' }).fill('Cathy.hettison@gmail.com');
-  //await page.getByRole('textbox', { name: 'Password* Password' }).click();
-  //await page.getByRole('textbox', { name: 'Password* Password' }).fill('Passis@123');
-  //await page.getByRole('button', { name: 'Sign In' }).click();
+  const Login = new LoginPage(page)
+  await Login.gotoLoginPage()
+  await Login.login('Mino@gmail.com','Mino@123')
+  await expect(page.getByText('The account sign-in was incorrect')).toBeVisible()
+ 
+});
+
+test('BlankEmail', async ({ page }) => {
+
+  const Login = new LoginPage(page)
+  await Login.gotoLoginPage()
+  await Login.login(' ','Lilani@123')
+  await expect(page.getByText('This is a required field')).toBeVisible()
+ 
+});
+
+test('BlankPassword', async ({ page }) => {
+
+  const Login = new LoginPage(page)
+  await Login.gotoLoginPage()
+  await Login.login('Yohani@outlool.com','')
+  await expect(page.getByText('This is a required field')).toBeVisible()
+ 
 });
